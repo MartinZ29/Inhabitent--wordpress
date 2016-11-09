ce3ece3e68c86af888ba3bbefeca206c0efb35ad
martinz <martinzhangyx@gmail.com>
2016-11-09 15:28:44 -0800

    addcontent

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -9,7 +9,20 @@
 
 <!-- shop stuff area !!!!!!!-->
 <section>
+    <h1>Shop stuff</h1>
+    <div class="shop-items">
+        <?php 
+$taxonomies = get_term( array(
+    'taxonomy' => 'product_type',
+    'hide_empty' => true,
+));
 
+ foreach ($taxonomies as $term):?>
+     <img src="<?php echo get_template_directory_uri() ?>/images/product-type-icons/<?php echo $term -> slug ?>.svg">
+     <p><?php echo $term -> description; ?></p>
+     <a href=""><?php echo $term -> name; ?></a>
+     <?php endforeach; wp_reset_postdata(); ?>
+    </div>
 </section>
 
 <!-- journal  area ! !!!!!!!!-->
@@ -17,5 +30,9 @@
 </section>
 
 
-<?php get_sidebar(); ?>
+<!-- adventure part !!!!!!!!!!-->
+<section>
+</section>
+
+
 <?php get_footer(); ?>
