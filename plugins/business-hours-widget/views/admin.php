3ffec77a1091430b16f3dd66c617f22b77a6464c
martinz <martinzhangyx@gmail.com>
2016-11-09 11:27:17 -0800

    widget monday-friday

--- plugins/business-hours-widget/views/admin.php
+++ plugins/business-hours-widget/views/admin.php
@@ -5,5 +5,8 @@
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>">
    </p>
 
+   <p><label for="<?php echo $this->get_field_id('monday_friday'); ?>">Monday to Friday:</label>
+		<input class="widefat" id="<?php echo $this->get_field_id('monday_friday'); ?>" name="<?php echo $this->get_field_name('monday_friday'); ?>" type="text" value="<?php echo $monday_friday; ?>">
+   </p>
 
 </div>
