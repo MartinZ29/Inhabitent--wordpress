4bd4d1969112fe52245f73c6be9e65c2dbbd034d
martinz <martinzhangyx@gmail.com>
2016-11-10 00:45:51 -0800

    fixfontawesome

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -85,7 +85,7 @@ add_filter( 'stylesheet_uri', 'red_starter_minified_css', 10, 2 );
 function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
 	//add font awsome here!!!!!
-	wp_enqueue_style('font-awsome-cdn','//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css');
+	wp_enqueue_style('font-awsome','//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css');
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
