58c9b47b0d9243ac79aadf887294b14279a31c17
martinz <martinzhangyx@gmail.com>
2016-11-10 15:40:01 -0800

    ARCHIVE FRONTCSS

--- themes/inhabitent/about.php
+++ themes/inhabitent/about.php
@@ -10,12 +10,19 @@ get_header(); ?>
 <div class="about-hero-banner"></div>
 
 <div class="about-content">
-    <?php $props = CFS()->get_field_info('our_story');
-    echo $props['label']; ?>
-    <?php echo CFS()->get('our_story'); ?>
+    <?php
+           $props = CFS()->get_field_info('our_story');
+           echo $props['label'];
+           ?>
+           <?php echo CFS()->get( 'our_story'); ?>
 
-    <?php $props = CFS()->get_field_info('our_team');
-    echo $props['label']; ?>
-    <?php echo CFS()->get('our_team'); ?>
+           <?php
+           $props = CFS()-> get_field_info('our_team');
+           echo $props['label'];
+           ?>
+
+           <?php 
+           echo CFS()->get( 'our_team');
+           ?>
     
 </div>
--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -6,11 +6,9 @@
  */
 
 get_header(); ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-		<?php if ( have_posts() ) : ?>
+			<?php if ( have_posts() ) : ?>
 			<header class="archive">
 				<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
@@ -20,7 +18,7 @@ get_header(); ?>
 			<?php $terms = get_terms('product_type'); ?> 
 			<?php foreach ($terms as $term) : ?> 
 			<div class='shop-stuff'>
-				<?php $url = get_term_link ($term->slug , 'product_type'); ?> 
+				<?php $url = get_term_link ($term->slug , 'product_type'); ?> +
 				<a href='<?php echo $url ?>' class='button'>
 					<?php  echo $term->name ?>
 				</a>
@@ -36,9 +34,11 @@ get_header(); ?>
 					<?php endwhile; ?>
 					<?php the_posts_navigation(); ?>
 					<?php else : ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-
-<?php get_footer(); ?>
+					<?php get_template_part( 'template-parts/content', 'none' ); ?>
+					<?php endif; ?>
+			</div>
+		</main>
+		<!-- #main -->
+		</div>
+		
+		<!-- #primary -->
