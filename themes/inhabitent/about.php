a5fcc8da1f2bfdf3435df3bce6671ef3ea87f465
martinz <martinzhangyx@gmail.com>
2016-11-10 00:11:15 -0800

    aboutpage

--- themes/inhabitent/about.php
+++ themes/inhabitent/about.php
@@ -1,4 +1,21 @@
-/*  
-Template name : About page
+<?php
+/**
+ * Tamplate Name: About page
+ *
+ * @package RED_Starter_Theme
+ */
 
-*/
+get_header(); ?>
+
+<div class="about-hero-banner"></div>
+
+<div class="about-content">
+    <?php $props = CFS()->get_field_info('our_story');
+    echo $props['label']; ?>
+    <?php echo CFS()->get('our_story'); ?>
+
+    <?php $props = CFS()->get_field_info('our_team');
+    echo $props['label']; ?>
+    <?php echo CFS()->get('our_team'); ?>
+    
+</div>
