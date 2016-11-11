df4a744fbf5c7baf86885dc68b5754ded08564a9
martinz <martinzhangyx@gmail.com>
2016-11-10 16:08:54 -0800

    fix single post

--- themes/inhabitent/single-post.php
+++ themes/inhabitent/single-post.php
@@ -7,12 +7,26 @@
 
 get_header(); ?>
 
-<div class="content-area">
-    <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('template-part/content','single'); ?>
-    <?php if (comments_open() || get_comments_number()) : comments_template(); endif; ?>
-    <?php endwhile; ?>
-</div>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+			
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+
+		<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
