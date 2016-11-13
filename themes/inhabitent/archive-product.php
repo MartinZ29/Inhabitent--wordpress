f5a7232dde6325b9eaaebded7dc698f763dcc1e1
martinz <martinzhangyx@gmail.com>
2016-11-13 14:00:37 -0800

    php format

--- themes/inhabitent/archive-product.php
+++ themes/inhabitent/archive-product.php
@@ -9,42 +9,42 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<div class="container">
-			<?php if ( have_posts() ) : ?>
-			<header class="archive">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-				?>
+				<?php if ( have_posts() ) : ?>
+				<header class="archive">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+					?>
 			
 			<!-- .page-header -->
-				<div class="shop-stuff-list">
-				<?php $terms = get_terms('product_type'); ?>
-				<?php foreach ($terms as $term) : ?> 
-				<div class='shop-stuff'>
-					<?php $url = get_term_link ($term->slug , 'product_type'); ?>
-					<a href='<?php echo $url ?>' class='button'>
-						<?php  echo $term->name ?>
-					</a>
-				</div> <!-- shop stuff -->
-			<?php endforeach; ?>
-				</div> <!--  shop stuff list -->
-			</header>
+					<div class="shop-stuff-list">
+						<?php $terms = get_terms('product_type'); ?>
+						<?php foreach ($terms as $term) : ?> 
+						<div class='shop-stuff'>
+							<?php $url = get_term_link ($term->slug , 'product_type'); ?>
+							<a href='<?php echo $url ?>' class='button'>
+								<?php  echo $term->name ?>
+							</a>
+						</div> <!-- shop stuff -->
+						<?php endforeach; ?>
+					</div> <!--  shop stuff list -->
+				</header>
 
-			<div id="primary" class="archive-area">
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
+				<div id="primary" class="archive-area">
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+					<?php
+						get_template_part( 'template-parts/content' );
+					?>
 					<?php endwhile; ?>
 					<?php the_posts_navigation(); ?>
 					<?php else : ?>
 					<?php get_template_part( 'template-parts/content', 'none' ); ?>
 					<?php endif; ?>
-			</div>
-			</div>
+				</div> <!-- archive area -->
+			</div> <!-- container -->
 		</main>
 		<!-- #main -->
-		</div>
+	</div> <!-- content-area -->
 		
 		<!-- #primary -->
 
