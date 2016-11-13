37d83536a3f086637af6c271e564ff26de956f19
martinz <martinzhangyx@gmail.com>
2016-11-12 21:03:12 -0800

    shop-items fixed

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -8,7 +8,7 @@
 
 
 <!-- shop stuff area !!!!!!!-->
-<section class="shop-list">
+<section class="shop-list container">
     <h1>Shop stuff</h1>
     <div class="shop-items">
         <?php 
@@ -16,9 +16,11 @@ $terms = get_terms('product_type');
 
  foreach ($terms as $term):?>
     <?php $url = get_term_link($term->slug,'product_type'); ?>
+    <div class="product-type">
      <img src="<?php echo get_template_directory_uri() ?>/images/product-type-icons/<?php echo $term->slug ?>.svg">
      <p><?php echo $term->description; ?></p>
-     <a href="<?php echo $url ?>"><?php echo $term->name.'Stuff'; ?></a>
+     <p><a href="<?php echo $url ?>" class="shop-botton"><?php echo $term->name.' Stuff'; ?></a></p>
+     </div>
      <?php endforeach; wp_reset_postdata(); ?>
     </div>
 </section>
