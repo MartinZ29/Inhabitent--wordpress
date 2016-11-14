c904aa99811e121fc45c836ed4d0082f9f32da17
martinz <martinzhangyx@gmail.com>
2016-11-13 17:51:31 -0800

    fix tempalte archive php

--- themes/inhabitent/template-parts/product-archive.php
+++ themes/inhabitent/template-parts/product-archive.php
@@ -7,23 +7,19 @@
 */
 ?>
 
-<div class = "product-post">
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-   <header class="entry-header">
-	<div class ="product-item"	>
 		<?php if ( has_post_thumbnail() ) : ?>
-			<a href = '<?php the_permalink();?>'> <?php the_post_thumbnail( 'large' ); ?> </a>
+		<div class="product-thumbnail"><a href = '<?php the_permalink();?>'> <?php the_post_thumbnail( 'large' ); ?> </a></div>
 		<?php endif; ?>
 		<div class = "item-price">
 			<?php the_title( sprintf( '<p class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></p>' ); ?>
-		  	<p><?php echo CFS()->get( 'product_price' ); ?><p>
+		  	<p><?php echo CFS()->get( 'product_price' ); ?></p>
 		</div>	 
-	</div>	 
-		<?php if ( 'post' === get_post_type() ) : ?>
+		 
+		 <?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
 		</div>
-		<?php endif; ?>
-	</header>
+		<?php endif; ?> 
 </article>
-</div>
