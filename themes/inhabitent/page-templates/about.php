6fdaccfd42d654c3db4dde8f000cd1998b73e2ca
martinz <martinzhangyx@gmail.com>
2016-11-13 23:13:00 -0800

    add about.css

--- themes/inhabitent/page-templates/about.php
+++ themes/inhabitent/page-templates/about.php
@@ -6,28 +6,23 @@
  */
 
 get_header(); ?>
-<div class="container">
-
         <div class="about-img">
             <h1>About</h1>
         </div>
-
+        <div class="container">
             <div class="about-content">
                 <?php
                 $props = CFS()->get_field_info('our_story');
                 echo $props['label'];
-            ?>
-            <?php echo CFS()->get( 'our_story'); ?>
+                ?>
+                <?php echo CFS()->get( 'our_story'); ?>
 
-            <?php
-            $props = CFS()-> get_field_info('our_team');
-            echo $props['label'];
-            ?>
-
-           <?php 
-           echo CFS()->get( 'our_team');
-           ?>
+                <?php
+                $props = CFS()-> get_field_info('our_team');
+                echo $props['label'];
+                ?>
+                <?php echo CFS()->get( 'our_team'); ?>
            </div>
-</div>
+        </div>
     
 <?php get_footer(); ?>
