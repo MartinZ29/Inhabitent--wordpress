ed058565f172f4c811ec6e24200203ef6d4c2b01
martinz <martinzhangyx@gmail.com>
2016-11-13 17:37:28 -0800

    fix product archive

--- themes/inhabitent/template-parts/product-archive.php
+++ themes/inhabitent/template-parts/product-archive.php
@@ -9,19 +9,21 @@
 
 <div class = "product-post">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-   
-        <?php if ( 'post' === get_post_type() ) : ?>
-        <div class="archive-meta">
-            <?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
-        </div><!-- .archive-meta -->
-        <?php endif; ?>
- 
-   		<?php if ( has_post_thumbnail() ) : ?>
-          <?php  echo '<a rel="bookmark" href="'. esc_url( get_permalink() ) .'"/a>';  ?>
-            <?php   the_post_thumbnail( 'large' ); ?>
-           
-            <?php the_title('<p class="archive-title"></h2>'); ?> 
-               ..........<?php echo CFS()->get( 'product_price' ); ?> </p>
-        <?php endif; ?>
-</article><!-- #post-## -->
+   <header class="entry-header">
+	<div class ="product-item"	>
+		<?php if ( has_post_thumbnail() ) : ?>
+			<a href = '<?php the_permalink();?>'> <?php the_post_thumbnail( 'large' ); ?> </a>
+		<?php endif; ?>
+		<div class = "item-price">
+			<?php the_title( sprintf( '<p class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></p>' ); ?>
+		  	<p><?php echo CFS()->get( 'product_price' ); ?><p>
+		</div>	 
+	</div>	 
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php red_starter_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php red_starter_posted_by(); ?>
+		</div>
+		<?php endif; ?>
+	</header>
+</article>
 </div>
