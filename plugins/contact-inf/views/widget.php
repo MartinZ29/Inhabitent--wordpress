7a882c80d6258ff10452fd3c00154ee6b89e49f1
martinz <martinzhangyx@gmail.com>
2016-11-21 16:35:03 -0800

    make phone and email a link!!!

--- plugins/contact-inf/views/widget.php
+++ plugins/contact-inf/views/widget.php
@@ -2,21 +2,21 @@
 
 <?php if( strlen( trim( $phone )) > 0) : ?>
 <p>
-    <i class="fa fa-phone" aria-hiddern="true"> </i> <?php echo $phone; ?>
+    <i class="fa fa-phone" aria-hiddern="true"><a href="tel:<?php echo $phone; ?>"> <?php echo $phone; ?></a></i> 
 </p>
 
 <?php endif; ?>
 
 <?php if( strlen( trim( $email)) > 0) : ?>
 <p>
-    <i class="fa fa-envelope"> </i> <?php echo $email; ?>
+    <i class="fa fa-envelope"><a href="mailto:<?php echo $email; ?>"> <?php echo $email; ?></a></i>
 </p>
 
 <?php endif; ?>
 
 <?php if( strlen( trim( $address )) > 0) : ?>
 <p>
-    <i class="fa fa-map-marker" aria-hidden="true"> </i> <?php echo $address; ?>
+    <i class="fa fa-map-marker" aria-hidden="true"> <?php echo $address; ?></i> 
 </p>
 
 <?php endif; ?>
