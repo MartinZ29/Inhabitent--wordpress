ba0e7ad5afe2ca1efd8e1bb4fd1c0bbf4f23b990
martinz <martinzhangyx@gmail.com>
2016-11-22 10:48:39 -0800

    add enqueue for script

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -87,6 +87,7 @@ function red_starter_scripts() {
 	//add font awsome here!!!!!
 	wp_enqueue_style('font-awsome','//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css');
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
+	wp_enqueue_script( 'search-button', get_template_directory_uri().'/build/js/script.min.js', array(), false, true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -6,6 +6,6 @@
 		<label>
 			<input type="search" class="search-field" placeholder="Type and hit enter..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
-		<input type="submit" class="screen-reader-text" value="Search"/>	
+		<input type="submit" class="screen-reader-text" value="Search">	
 	</fieldset>
 </form>
