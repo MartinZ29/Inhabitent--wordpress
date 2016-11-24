9f8ee438aec308b46897fa81129b4be3a663fff6
martinz <martinzhangyx@gmail.com>
2016-11-23 17:29:14 -0800

    add sidebar to archive blog page

--- themes/inhabitent/archive.php
+++ themes/inhabitent/archive.php
@@ -6,7 +6,7 @@
  */
 
 get_header(); ?>
-
+<div class="content-container">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -39,5 +39,6 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-
+<?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
